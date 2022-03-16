412a27a58d4386e2e88e435b217b475c742a8412
luiisperez <luis.alejandro120895@gmail.com>
2022-03-16 16:07:16 +0100

    Last unit test

--- src/EmployeesIncidentsUnitTest/RestUnitTest.cs
+++ src/EmployeesIncidentsUnitTest/RestUnitTest.cs
@@ -1,7 +1,9 @@
 using EmployeesIncidents.Controllers;
 using EmployeesIncidents.Model.DataManagement;
+using EmployeesIncidents.Model.Objects;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 
 namespace EmployeesIncidentsUnitTest
@@ -12,7 +14,8 @@ namespace EmployeesIncidentsUnitTest
         [SetUp]
         public void Setup()
         {
-            dataLayer = new DataLayer();
+            if (dataLayer == null)
+                dataLayer = new DataLayer();
         }
 
         [Test]
@@ -38,6 +41,27 @@ namespace EmployeesIncidentsUnitTest
                 "Incidencia", new EmployeesIncidents.Model.Objects.Employee("TEST", "TEST", "TEST")));
 
             Assert.AreEqual("EMPLOYEE DOES NOT EXIST", messageEmployeeError.ReasonPhrase);
+        }
+
+        [Test]
+        public void TestIncidenceByEmployee()
+        {
+            var controller = new AssignmentController();
+            controller.Post(new EmployeesIncidents.Model.Objects.Assignment("RTR-8151", "TEST", "TEST", DateTime.Now,
+                "Incidencia", new EmployeesIncidents.Model.Objects.Employee("29419376F", "TEST", "TEST")));
+            controller.Post(new EmployeesIncidents.Model.Objects.Assignment("RTR-8152", "TEST", "TEST", DateTime.Now,
+                "Incidencia", new EmployeesIncidents.Model.Objects.Employee("29419376F", "TEST", "TEST")));
+            controller.Post(new EmployeesIncidents.Model.Objects.Assignment("RTR-8153", "TEST", "TEST", DateTime.Now,
+                "Incidencia", new EmployeesIncidents.Model.Objects.Employee("29419376F", "TEST", "TEST")));
+
+
+            List<Assignment> list = controller.GetIncidentsByEmployee("29419376F");
+
+            Assert.AreEqual(3, list.Count);
+
+            Assert.AreEqual("RTR-8151", list[0].Code);
+            Assert.AreEqual("RTR-8152", list[1].Code);
+            Assert.AreEqual("RTR-8153", list[2].Code);
         }
     }
 }
